2566ae444b10ec0ca03ba6eb66c21f30407b2daa
SwastikaJawahar <136087727+SwastikaJawahar@users.noreply.github.com>
2024-04-02 18:28:32 +0100

    Seeding data into DB

---
+++ app/lib/placeholder-data.js
@@ -0,0 +1,25 @@
+const movies = [
+  {
+    id: 1,
+    adult: false,
+    backdrop_path: "/sR0SpCrXamlIkYMdfz83sFn5JS6.jpg",
+    genre_ids: [3], // Example genre ids
+    original_language: "en",
+    original_title: "Godzilla x Kong: The New Empire",
+    overview:
+      "Following their explosive showdown, Godzilla and Kong must reunite against a colossal undiscovered threat hidden within our world, challenging their very existence – and our own.",
+    popularity: 5072.084,
+    poster_path: "/gmGK5Gw5CIGMPhOmTO0bNA9Q66c.jpg",
+    release_date: "2024-03-27",
+    title: "Godzilla x Kong: The New Empire",
+    video: false,
+    vote_average: 7.8,
+    vote_count: 236,
+  },
+
+  // Add more movie objects as needed
+];
+
+module.exports = {
+  movies,
+};
